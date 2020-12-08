8935742389ec69238cea2c4126dcd0707d94d166
hafedh <hafed.ammani@esprit.tn>
2020-12-08 21:38:24 +0100

    creation l'interface ajouter film

--- src/app/add-movies/add-movies.component.ts
+++ src/app/add-movies/add-movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { movies } from '../model/movies';
 
 @Component({
   selector: 'app-add-movies',
@@ -8,14 +9,37 @@ import { FormControl } from '@angular/forms';
 })
 export class AddMoviesComponent implements OnInit {
 
-  
+  moviesform : FormGroup;
+  fbuilder : FormBuilder = new FormBuilder();
+  m : movies = new movies();
+
+  id = new FormControl('');
+  title = new FormControl('');
+  duree = new FormControl('');
+  categorie = new FormControl('');
+  rate = new FormControl('');
 
   constructor() { }
 
   ngOnInit(): void {
-   
+    this.moviesform = this.fbuilder.group({
+      id : ['',Validators.required],
+      title : ['',Validators.required],
+      duree :  ['',[Validators.required, Validators.pattern("[1-9]")]],
+      categorie : ['',[Validators.required, Validators.minLength(3)]],
+      rate :  ['',[Validators.required, Validators.pattern("[1-9]")]]
+    })
   }
 
-  
+
+  resetControls(): void {
+    this.moviesform.patchValue({
+      id : null,
+      title : null,
+      duree : null,
+      categorie: null,
+      rate : null ,
+    })
+  }
 
 }
